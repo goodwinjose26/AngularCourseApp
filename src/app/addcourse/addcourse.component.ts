9b3e9fd0f96276f6d527c46b77af3729a1187e1a
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-28 12:26:51 +0530

    readvalue

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./addcourse.component.css']
 })
 export class AddcourseComponent {
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  readValues=()=>
+  {
+    let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+    console.log(data)
+  }
+
+
 
 }
